f9ea2931d942114f0862d39a785461d3a8750866
vishalvkhoday <vishalvkhoday@gmail.com>
2019-07-14 22:09:46 +0530

    14jly

--- src/Bse_Extractor_V1.py
+++ src/Bse_Extractor_V1.py
@@ -17,14 +17,20 @@ import os
 from selenium.webdriver.support import expected_conditions as EC
 
 
-Bse_file = "C:/Users/vkhoday/git/Selenium_NSE_Algo/Additonal_Utility/Bse_Res_Script_01Feb2019.xlsx"
+Bse_file = "C:/Users/DELL/git/Selenium_NSE_Algo/Additonal_Utility/Bse_Res_Script_14Apr2019.xlsx"
 driver = Obj_R.driver
 Wb = load_workbook(Bse_file)
 WsScripts = Wb['Sheet1']
 Ws_Result = Wb['Results']
 
 class Test(unittest.TestCase):
-
+    def clear_Temp (self):
+            
+        try:
+            os.system('del /f/s/q %temp%\*')
+        except AssertionError as ae:
+            print('Unable to delete temp files')
+    
     def setUp(self):
         driver.get("https://www.bseindia.com")
         driver.maximize_window()
@@ -38,8 +44,7 @@ class Test(unittest.TestCase):
     
     def testName(self):
         win_hand =driver.window_handles
-        print ('{}'.format(win_hand))        
-       
+        print ('{}'.format(win_hand))       
         rowCnt = WsScripts.max_row
         if rowCnt ==1:
             rowCnt=2
@@ -62,22 +67,45 @@ class Test(unittest.TestCase):
             exe_st = WsScripts[col_st].value            
             print ("{}) {}  {}".format(i,script_name,exe_st))
             if exe_st == 'Yes':
-                Bse_sctTxt = driver.find_element_by_xpath(Obj_R.xpath_scr_txt)                
-                Bse_sctTxt.send_keys(INIE)
-                time.sleep(3)                
-                driver.switch_to_default_content
-                Bse_sctTxt.send_keys(Keys.ENTER)
-                time.sleep(1)
-                driver.find_element_by_xpath(Obj_R.xpath_scr_txt).clear()
-                tblHeader =driver.find_element_by_xpath(Obj_R.xpath_tblHeader).get_attribute('innerText')
-                if tblHeader.find('Dec-18') == -1:
-                    continue
-                else:
-                    ActionChains(driver).click('//*[@id="tabres"]')
-                    time.sleep(2)
+                try:
+                    Bse_sctTxt = driver.find_element_by_xpath(Obj_R.xpath_scr_txt)                
+                    Bse_sctTxt.send_keys(INIE)
+                    time.sleep(3)                
+#                     driver.switch_to_default_content
+                    Bse_sctTxt.send_keys(Keys.ENTER)
+                    time.sleep(1)
+                    driver.find_element_by_xpath(Obj_R.xpath_scr_txt).clear()
+                    secID=""
+                    sec_code = ""
+                    sec_ISIN = ""
+                    scr_info =None
+                    scr_info =driver.find_element_by_xpath(Obj_R.xpath_script_ID_INIE).get_attribute('innerText')
+                    scr_info = scr_info.replace("(","")
+                    scr_info = scr_info.replace(")","")
+                    temp_scrId = str(scr_info).split("|")
+                    if str(temp_scrId[2]).strip() != str(INIE).strip():
+#                         driver.switch_to_default_content
+                        driver.find_element_by_xpath(Obj_R.xpath_scr_txt).click()
+                        driver.find_element_by_xpath(Obj_R.xpath_scr_txt).send_keys(script_name)
+#                         Bse_sctTxt.send_keys(script_name)
+                        driver.find_element_by_xpath(Obj_R.xpath_scr_txt).send_keys(Keys.ENTER)
+                        driver.find_element_by_xpath(Obj_R.xpath_scr_txt).clear()
+                        time.sleep(2)
+                        
+                        
+                    driver.find_element_by_xpath(Obj_R.xpath_tblHeader).location_once_scrolled_into_view
+                    tblHeader =driver.find_element_by_xpath(Obj_R.xpath_tblHeader).get_attribute('innerText')
+                    if tblHeader.find('Mar-19') == -1:
+                        continue
+                    else:
+                        ActionChains(driver).click('//*[@id="tabres"]')
+                        time.sleep(2)
+                
+                except:
+                    pass
                 
                 try:
-                    print('Enter try catch')
+#                     print('Enter try catch')
                     elm_res_bt = driver.find_element_by_xpath(Obj_R.xpath_res_bt)
                     time.sleep(1)
                     ActionChains(driver).move_to_element(elm_res_bt).click(elm_res_bt).perform()
@@ -87,29 +115,14 @@ class Test(unittest.TestCase):
                     
                 except:
 #                     driver.refresh()
+                    print("Error occured")
                     pass
-#                
-#                 try:
-#                     #Bse_sctTxt.send_keys(Keys.ENTER)
 
-#                     os.system("C:/Users/vkhoday/git/Selenium_NSE_Algo/Additonal_Utility/Enter.vbs")
-#                 except:
-#                     print('Enter key occured')
-#                     None
-#                 time.sleep(3)
-#                 try:                              
-#                     driver.find_element_by_xpath(Obj_R.xpath_res_bt).click()
-#                     time.sleep(3)
-#                     href= driver.find_element_by_xpath('//*[@id="res"]/div/div[1]/table/tbody[7]/tr/td[2]/a').get_attribute('href')
-#                     print(href)
-#                 except:
-#                     #href = driver.find_element_by_xpath(Obj_R.xpath_href_lk).get_attribute('pathname')
-#                     #driver.get(href)
-#                     None
                 try:
+                    driver.find_element_by_xpath(Obj_R.xpath_res_tbl).location_once_scrolled_into_view
                     t_Tbl_details = driver.find_element_by_xpath(Obj_R.xpath_res_tbl).get_attribute('innerText')
                     t_Tbl_details = t_Tbl_details.replace("Income Statement", "").replace("%", "")
-                    if t_Tbl_details.find('Dec-18')<0:
+                    if t_Tbl_details.find('Mar-19')<0:
 #                         temp_row = 'N'+ str(i)
 #                         WsScripts[temp_row]="No"
 #                         Wb.save(Bse_file)
@@ -122,21 +135,24 @@ class Test(unittest.TestCase):
                     Res_cnt = Ws_Result.max_row
                     if Res_cnt ==1:
                         Res_cnt =2
+                    
                     secID=""
-                    secID = driver.find_element_by_xpath(Obj_R.xpath_script_id).get_attribute('innerText')
-                    secID = secID.replace("Security ID :","").strip()
                     sec_code = ""
-                    sec_code = driver.find_element_by_xpath(Obj_R.xpath_Sec_code).get_attribute('innerText')
-                    sec_code = sec_code.replace("Security Code :", "").strip()
                     sec_ISIN = ""
-                    sec_ISIN = driver.find_element_by_xpath(Obj_R.xpath_INIE).get_attribute('innerText')
-                    sec_ISIN = sec_ISIN.replace("ISIN :", "").strip()
+                    scr_info =None
+                    scr_info =driver.find_element_by_xpath(Obj_R.xpath_script_ID_INIE).get_attribute('innerText')
+                    scr_info = scr_info.replace("(","")
+                    scr_info = scr_info.replace(")","")
+                    temp_scrId = str(scr_info).split("|")
+                    temp_scrId = temp_scrId
+                    secID = temp_scrId[0]
+                    sec_code =temp_scrId[1]
+                    sec_ISIN = temp_scrId[2]
+                    
                     if str(sec_ISIN.strip()) != str(INIE).strip():
                         continue
                 except:
                     continue
-                #t_Tbl_details = t_Tbl_details.replace('(in Cr.)', '')
-                
                 
                 try:
                     for rows in range(0,len(spt_Tbl_details)-1):
@@ -149,19 +165,18 @@ class Test(unittest.TestCase):
                                 else:
                                     Ws_Result['A'+ str(Res_cnt)]=script_name
                                     Ws_Result['B'+ str(Res_cnt)]=sec_ISIN
-                                    Ws_Result['C'+ str(Res_cnt)]=spt_row[0]
-                                    Ws_Result['D'+ str(Res_cnt)]=spt_row[1]
-                                    Ws_Result['E'+ str(Res_cnt)]=spt_row[2]
-                                    Ws_Result['F'+ str(Res_cnt)]=spt_row[3]
-                                    Ws_Result['G'+ str(Res_cnt)]=spt_row[4]
-                                    Ws_Result['H'+ str(Res_cnt)]=spt_row[5]
+                                    Ws_Result['C'+ str(Res_cnt)]=str(spt_row[0])
+                                    Ws_Result['D'+ str(Res_cnt)]=str(spt_row[1])
+                                    Ws_Result['E'+ str(Res_cnt)]=str(spt_row[2])
+                                    Ws_Result['F'+ str(Res_cnt)]=str(spt_row[3])
+                                    Ws_Result['G'+ str(Res_cnt)]=str(spt_row[4])
+                                    Ws_Result['H'+ str(Res_cnt)]=str(spt_row[5])
                                     Res_cnt+=1
-                                                            
+                                                        
                             except:
                                 temp_row = 'N'+ str(i)
                                 WsScripts[temp_row]="No"
                                 Wb.save(Bse_file)
-                                
                         
                     temp_row = 'N'+ str(i)
                     WsScripts[temp_row]="No"
@@ -173,10 +188,8 @@ class Test(unittest.TestCase):
                     WsScripts[temp_row]="No"
                     Wb.save(Bse_file)
                     continue
-                
-        
-
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
+    
--- src/Object_repository/__init__.py
+++ src/Object_repository/__init__.py
@@ -2,15 +2,19 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 
 
-driver = webdriver.Chrome('c:/Python36/chromedriver_242')
+option =webdriver.ChromeOptions()
+option.add_argument("start-maximized")
+option.add_argument("disable-infobars")
+driver = webdriver.Chrome(chrome_options=option,executable_path='C:/Users/DELL/git/Selenium_NSE_Algo/Additonal_Utility/chromedriver_242')
 #driver = webdriver.Ie('c:/Python36/chromedriver_242')
 
 xpath_scr_txt = '//*[@id="getquotesearch"]'
 xpath_res_bt = '//*[@id="res"]/div/div[1]/table/tbody'
 xpath_href_lk = '//*[@id="res"]/div/div[1]/table/tbody[7]/tr/td[2]/a'
-# xpath_res_tbl = '//*[@id="qtly"]/table/tbody/tr/td/table[1]'
-xpath_res_tbl = '//*[@id="divmain"]/div[2]/div[2]/div/div/div[2]/div'
+xpath_res_tbl = '//*[@id="qtly"]/table/tbody/tr/td/table[1]'
+# xpath_res_tbl = '//*[@id="divmain"]/div[2]/div[2]/div/div/div[2]/div'
 xpath_script_id = '/html/body/div[3]/div[4]/div/div/div[2]/div/div[1]'
+xpath_script_ID_INIE = '//*[@id="eqcommanheader"]/div[3]/div/div[1]/div[1]/div[1]/div[2]/div/div[3]'
 xpath_INIE = '/html/body/div[3]/div[4]/div/div/div[2]/div/div[5]'
 xpath_Ind = '/html/body/div[3]/div[3]/div/div/div[2]/div/div[6]'
 xpath_Sec_code = '/html/body/div[3]/div[4]/div/div/div[2]/div/div[4]'
