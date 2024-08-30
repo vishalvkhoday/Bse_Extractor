2cf8abe15aaccc5cbeba4c70d66b0d5a7ccfa15e
vishalvkhoday <vishalvkhoday@gmail.com>
2024-08-30 20:41:22 +0530

    PowerBI script added

---
+++ src/PowerBI.py
@@ -0,0 +1,19 @@
+# Prolog - Auto Generated #
+import os, uuid, matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot
+import pandas
+
+import sys
+sys.tracebacklimit = 0
+
+os.chdir(u'C:/Users/Vishal/PythonEditorWrapper_4e5ed29b-8ae4-47bd-a36e-1c1db2654222')
+dataset = pandas.read_csv('input_df_56fdb15c-cf4c-4a10-a2e7-b6cfbe9f3df2.csv')
+
+matplotlib.pyplot.figure(figsize=(5.55555555555556,4.16666666666667), dpi=72)
+matplotlib.pyplot.show = lambda args=None,kw=None: matplotlib.pyplot.savefig(str(uuid.uuid1()))
+# Original Script. Please update your script content here and once completed copy below section back to the original editing window #
+##############################
+
+# Epilog - Auto Generated #
+os.chdir(u'C:/Users/Vishal/PythonEditorWrapper_4e5ed29b-8ae4-47bd-a36e-1c1db2654222')
